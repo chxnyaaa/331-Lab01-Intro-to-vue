7e38b9c5251e5391afcc9bf9fde477a0933d6a42
Chanyanuch <chanyanuch_panyadee@cmu.ac.th>
2024-06-26 20:56:17 +0700

    4.4 inStock to false

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ createApp({
         const description = ref('This is boots')
         const image = ref('./assets/images/socks_green.jpg')
         const link = ref('https://www.camt.cmu.ac.th/index.php/th/')
-        const inStock = ref(true)
+        const inStock = ref(false)
         return {
             product, 
             description, 

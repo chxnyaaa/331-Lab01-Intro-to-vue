ca47e680a0cf28e5555d678b0644cf2724af8b38
Chanyanuch <chanyanuch_panyadee@cmu.ac.th>
2024-06-26 21:01:51 +0700

    4.8 chain logic

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ createApp({
         const image = ref('./assets/images/socks_green.jpg')
         const link = ref('https://www.camt.cmu.ac.th/index.php/th/')
         const inStock = ref(true)
-        const inventory = ref(100)
+        const inventory = ref(10)
         return {
             product, 
             description, 

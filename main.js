54aeec18cae85e70b53a97b56b30eeeac956024c
Chanyanuch <chanyanuch_panyadee@cmu.ac.th>
2024-06-26 20:48:56 +0700

    4.2 Add condition inStock

--- main.js
+++ main.js
@@ -5,8 +5,13 @@ createApp({
         const description = ref('This is boots')
         const image = ref('./assets/images/socks_green.jpg')
         const link = ref('https://www.camt.cmu.ac.th/index.php/th/')
+        const inStock = ref(true)
         return {
-            product, description, image, link
+            product, 
+            description, 
+            image, 
+            link,
+            inStock
         }
     }
 }).mount('#app')

2eb2d03f76f739c929b4b152068fa7b3570e5e9d
Chanyanuch <chanyanuch_panyadee@cmu.ac.th>
2024-06-26 19:50:30 +0700

    3.2 3.3 add image (not show)

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ createApp({
     setup(){
         const product = ref('Boots')
         const description = ref('This is boots')
+        const image = ref('./assets/images/socks_green.jpg')
         return {
-            product, description
+            product, description, image
         }
     }
 }).mount('#app')

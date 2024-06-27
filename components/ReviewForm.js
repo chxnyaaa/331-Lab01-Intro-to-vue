a67905a1fc864f7aee8defc14e9d9bd638eb88a8
Chanyanuch <chanyanuch_panyadee@cmu.ac.th>
2024-06-27 22:09:58 +0700

    11.3 edit ReviewForm.js

--- components/ReviewForm.js
+++ components/ReviewForm.js
@@ -2,13 +2,13 @@ const reviewForm = {
     template: `<form class="review-form">
     <h3>Leave a review</h3>
     <label for="name">Name:</label>
-    <input id="name">
+    <input id="name" v-model="form.name">
 
     <label for = "review">Review:</label>
-    <textarea id="review"></textarea>
+    <textarea id="review" v-model="form.review"></textarea>
 
     <label for="rating">Rating:</label>
-    <select id="rating">
+    <select id="rating" v-model.number="form.rating>
         <option>5</option>
         <option>4</option>
         <option>3</option>

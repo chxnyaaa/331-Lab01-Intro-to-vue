3d13620fe31f75e28c5bb03e068c90403e3230b8
Chanyanuch <chanyanuch_panyadee@cmu.ac.th>
2024-06-27 15:53:10 +0700

    9.3 import ProductDisplay.is

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const { createApp, ref, computed } = Vue 
-createApp({
+const app = createApp({
     setup(){
         const cart = ref(0)
         return {
             cart,
         }
     }
-}).mount('#app')
+})
+app.component('product-display', productDisplay)
+app.mount('#app')
